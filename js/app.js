00bc1f2f6218070784e4f63d0a316fb52a902971
Manzambi Bissengo Matila <bissengo1991@gmail.com>
2018-08-07 13:37:21 +0200

    second commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,46 @@
 // le model
 let model = {
+    nombreDeLigne: 15,
+    nombreDeColonne: 16,
+
+    // creation d'un tableau
+    creerTableau: function () {
+        let tableau = document.getElementById('tableau');
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        for(let i = 0; i < this.nombreDeLigne; i ++) {
+            tr.appendChild(td)
+            for(let j = 0; j < this.nombreDeColonne; j++) {
+
+            }
+        }
+        }
+        
+
+    }
+
+
+
+
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -17,5 +56,5 @@ let view = {
 
 //l'Octopus
 let controller = {
-    
+
 }
