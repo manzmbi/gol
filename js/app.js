ae682af937b3b31127e6dad61b163310c14b6da0
Manzambi Bissengo Matila <bissengo1991@gmail.com>
2018-09-06 04:23:07 +0200

    tableau virtuel

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 
+ // Nombre des lignes et ceux des colonnes
+ let nombreDeLigne = 50;
+ let nombreDeColonne = 50;
+ 
 creerTable = function(){
-
-  // Nombre des lignes et ceux des colonnes
-  let nombreDeLigne = 50;
-  let nombreDeColonne = 50;
-  
   // Acceder a l'element Body
   let body = document.getElementsByTagName("body");
 
@@ -39,39 +38,144 @@ creerTable = function(){
 
 creerTable()
 
-// changer la couleur 
-let td = document.getElementsByTagName('td');
-for ( let i = 0; i < td.length; i++) {
-  let tdindex = td[i];
-  tdindex.addEventListener('click', function () {
-      tdindex.classList.toggle('mort')
-    })
+
+
+// changer la couleur des cellules en cliquant
+changerColeurCellule = function () {
+  let td = document.getElementsByTagName('td');
+  for ( let i = 0; i < td.length; i++) {
+    td[i].addEventListener('click', function () {
+        td[i].classList.toggle('mort')
+      })
+  }
 }
 
+changerColeurCellule()
 
-// changer le contenue du bouton start en pause
+
+
+// changer le contenue du premier bouton start en pause <<==>> continue
+
+let changerContenuBouton1 = function () {
+// Acceder a l'element bouton
 let bouton = document.querySelector('.bouton');
-// Acceder a l'element
+// Ajouter un ecouteur puis l'evenement
 bouton.addEventListener('click', function() {
+  //Verification contenu bouton Start en Pause
 if (bouton.textContent === 'Start'){
   bouton.textContent = 'Pause';
-  console.log('Pause');
 }
-
+  //Verification contenu bouton Pause en Continue
 else if (bouton.textContent === 'Pause'){
-  bouton.textContent = 'Continue';
-  console.log('Continue');
-  
+  bouton.textContent = 'Continue';  
 }
-
+  //Verification contenu bouton continue en pause
 else if (bouton.textContent === 'Continue'){
   bouton.textContent = 'Pause';
-  console.log('Pause');
-  
+}
+})
 }
 
+changerContenuBouton1()
 
+
+
+
+ 
+// La fonction : Generer aleatoirement les cellules mortes et vivantes
+let genereAleatoireBinaire = function() {
+  let td = document.getElementsByTagName('td');
+  for ( let i = 0; i < td.length; i++) {
+    // let x = Math.round(Math.random());
+    // Changer les cellules qui contienent 1 en noir
+    // x === 0 ? td[i].className = "mort" : td[i].className = "vivant";
+    td[i].className = Math.round(Math.random()) === 0 ? "mort" : "vivant"; 
+    // if (x === 0){
+    //   td[i].className = "mort";
+    //   }
+    // // Changer les cellules qui contienent 0 en blanc
+    // else {
+    //   td[i].className = "vivant";
+    //   }
+      
+    }
+  }
+
+// Clique sur le bouton random, Appeler la fonction genereAleatoireBinaire
+let boutonRandom = document.querySelectorAll('.bouton')[1]
+boutonRandom.addEventListener('click',function() {
+  genereAleatoireBinaire()
 })
+
+
+
+
+
+// creation d'un tableau virtuel
+
+let createTableauVirtuel = function() {
+  let tableauVirtuel = [];
+  // recuperer le tableau physique HTML
+  let tableauNavigateur = document.getElementById('table');
+  //parcourir toutes les lignes
+  for (var i = 0; i < nombreDeLigne.length; i++) {
+    let tr = document.getElementsByTagName('tr');
+    let trs = tr[i];
+    // creation d'un array vide qui contiendra les 'tr virtuel'
+    let trVirtuel = []
+    // ajouter les 'tr virtuel' dans le tableau virtuel
+    tableauVirtuel.push(trVirtuel);
+
+    // Parcourir et Ajouter les donnees des cellules dans le 'tr virtuel'
+    for (var j = 0; i < nombreDeColonne.length; j++) {
+    let td = document.getElementsByTagName('td');
+
+       // verification
+      // if (td[j].className === 'mort' ) {
+      //   trVirtuel.push(1);
+      // }  
+      // else {
+      //   trVirtuel.push(0);
+      // }
+      td[j].className === 'vivant' ? trVirtuel.push(1) : trVirtuel.push(0);
+    }
+  }
+  return tableauVirtuel;
+}
+createTableauVirtuel()
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// clique sur bouton reset pour reset
+let boutonReset = document.querySelectorAll('.bouton')[2]
+boutonReset.addEventListener('click',function() {
+  
+})
+
 
 
 
